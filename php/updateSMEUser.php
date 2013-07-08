5777c2f628da58264338653deb480f78a7c4281b
thalsuzinwin <thalsuzinwin@9addf9c1-fd18-f9e5-0997-70d331cf753b>
2013-07-08 16:45:25 +0000

    for login page

--- php/updateSMEUser.php
+++ php/updateSMEUser.php
@@ -16,28 +16,26 @@ $user->setPassword($_POST["password"]);
 $user->setPhoneNo($_POST["phoneNumber"]);
 $em->persist($user);
 $em->flush();
-$transport = Swift_SmtpTransport::newInstance('relay-hosting.secureserver.net', 25);
-
-
-
+$msg = "Your account has been successfully updated please logout and login again to see the changes.";
+/* $transport = Swift_SmtpTransport::newInstance('relay-hosting.secureserver.net', 25);
 // Create the Mailer using your created Transport
 $mailer = Swift_Mailer::newInstance($transport);
-
 // Create a message
 $message = Swift_Message::newInstance('NoRely-yourmyanmarsme-com')
   ->setFrom(array('noreply@yourmyanmarsme.com' => 'MMSME-NoReply'))
   ->setTo(array($user->getEmail() => $user->getFullName()))
   ->setBody("Greetings, ".$user->getFullName()."\n\nThank you for your interest in Myanmar SME Project.\n\nYou are receiving this email because someone has started a registraion on our website \"www.yourmyanmarsme.com\" using the email address\n".$user->getEmail()." and a user name ".$user->getFullName().".\n\n\n\nBest Regards\nAdmin")
   ;
-
 // Send the message
 //$result = $mailer->send($message);
-echo '{"result" : 0}';
-
+	echo '{"result" : 0}'; */
+	$success = 1;
 } catch (Exception $e) {
-
   $log->LogInfo($e->getMessage());
-   echo '{"result" : 1}';
+  //echo '{"result" : 1}';
+  $msg = $e->getMessage();
+  $success = 0;
 }
+$arr = array('success'=>$success, 'msg'=> $msg);
+echo json_encode($arr);
 ?>
-
