044ae46cb8851d4becd9a211d6728b540b182c7e
nanda.ag@gmail.com <nanda.ag@gmail.com@9addf9c1-fd18-f9e5-0997-70d331cf753b>
2013-07-30 14:16:23 +0000

    28-07-2013

---
+++ php/include/table_connection.php
@@ -0,0 +1,14 @@
+<?php
+	require_once 'commonutil.php';
+	require_once 'entities/User.php';
+	require_once 'entities/SMEUser.php';
+	require_once 'entities/IndustrialZone.php';
+	require_once 'entities/SMECompany.php';
+	require_once 'entities/NewsFeed.php';
+	require_once 'entities/Tags.php';
+	require_once 'entities/UserDirectory.php';
+	require_once 'entities/UserDirectory1.php';
+	require_once 'entities/DirectoryTag.php';
+	require_once 'entities/DirectoryTag1.php';
+	require_once 'entities/DirectoryCompany.php';
+?>
